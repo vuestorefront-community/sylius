6f02add657e440efa9aa8508138754777762b209
dead-playmod <playmod.pl@gmail.com>
2022-08-26 15:53:15 +0200

    chore: change import paths

--- packages/composables/src/getters/cartGetters.ts
+++ packages/composables/src/getters/cartGetters.ts
@@ -1,5 +1,5 @@
 import { CartGetters, AgnosticPrice, AgnosticTotals, AgnosticCoupon, AgnosticDiscount } from '@vue-storefront/core';
-import { Cart, CartLineItem } from '@realtainment/sylius-api/src/types';
+import { Cart, CartLineItem } from '@vue-storefront/sylius-api/src/types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getCartItems = (cart: Cart): CartLineItem[] => {
